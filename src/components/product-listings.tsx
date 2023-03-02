9b2847479971d3b6499d02199006802a6cb8ae5d
Simon Lam <thesimonlam@gmail.com>
2023-03-02 22:24:15 +0000

    infinite scroll

--- src/components/product-listings.tsx
+++ src/components/product-listings.tsx
@@ -6,8 +6,15 @@ import css from "./product-listings.module.css";
 export const ProductListings = () => {
   const products = useSelector((state: RootState) => state.products.products);
 
+  const handleScroll = (e: any) => {
+    const target = e.target;
+    if(target.scrollHeight - target.scrollTop === target.clientHeight){
+      console.log('whoe')
+    }
+  }
+
   return (
-    <div className={css.productListings}>
+    <div className={css.productListings} onScroll={handleScroll}>
       {products?.map((product, key) => (
         <Product key={key} product={product}></Product>
       ))}
